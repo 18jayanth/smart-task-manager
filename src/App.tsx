1c0ff511bc049bdca51b0f351c51d7ebd107d6d9
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-05-26 10:57:02 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Smart Task Manager</h1>
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
